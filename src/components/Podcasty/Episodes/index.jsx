707453c1a7e8c3becebbe2c92613ac9b418476ea
Nikola Slunečková <sluneckova.nikola@gmail.com>
2025-06-04 11:50:57 +0200

    1

---
+++ src/components/Podcasty/Episodes/index.jsx
@@ -0,0 +1,14 @@
+
+export const Episodes = (props) => {
+  return (
+    <div className="episodes-list">
+      <div className="episode">
+        <div className="episode__num">{props.num}</div>
+        <div className="episode__body">
+          <div className="episode__title">{props.title}</div>
+          <div className="episode__guest">{props.guest}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
